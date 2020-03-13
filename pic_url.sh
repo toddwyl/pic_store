21772b8d85a79a94cf11877b3240d5fb787ff724
ManWingloeng <strawsuper@gmail.com>
2020-03-13 22:31:27 +0800

    update pic_url.sh

--- pic_url.sh
+++ pic_url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "please input md_file and we will output the convert img_url md_file"
-echo "$1 --> $2"
+echo "$2 --> $3"
 github_url='https://raw.githubusercontent.com/ManWingloeng/pic_store/master/'
 jsdelivr_url='https://cdn.jsdelivr.net/gh/ManWingloeng/pic_store@master/'
-sed -e "s#CVPR2020.assets/#${jsdelivr_url}/CVPR2020.assets/#g" $1> $2
+sed -e "s#$1/#${jsdelivr_url}/$1/#g" $2> $3
